60357f986cb60fd8059388aaa4ba0dde71c0d504
miki_kosaka <milk.121.miki@gmail.com>
2024-12-12 03:13:54 +0900

    feature:MainActivitty実装

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,9 @@ plugins {
 android {
     namespace = "com.example.myapplication"
     compileSdk = 34
-
+    buildFeatures {
+        viewBinding = true
+    }
     defaultConfig {
         applicationId = "com.example.myapplication"
         minSdk = 24
