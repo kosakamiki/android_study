1f6718cf8dbac967e98d89c95ca831605013e00b
miki_kosaka <milk.121.miki@gmail.com>
2025-07-03 03:12:40 +0900

    feature/ボタンの位置調整、ボタンの文言調整、ボタンを動かせるようにした（タップが効かなくなってしまった）

--- app/src/main/java/com/example/myapplication/WebViewActivity.kt
+++ app/src/main/java/com/example/myapplication/WebViewActivity.kt
@@ -3,7 +3,9 @@ package com.example.myapplication
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.view.MotionEvent
 import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
@@ -15,6 +17,36 @@ class WebViewActivity : AppCompatActivity() {
         setContentView(R.layout.web_view_screen)
 
         val webViewButton: Button = findViewById(R.id.webViewBtn)
+        // タッチする前のViewの位置を常に持っておきたいため、TouchListener外でインスタンス化
+        var oldPositionX = 0
+        var oldPositionY = 0
+        webViewButton.setOnTouchListener { view, event ->
+
+            val newPositionX = event.rawX.toInt()
+            val newPositionY = event.rawY.toInt()
+
+            when (event.action) {
+                MotionEvent.ACTION_MOVE -> {
+                    // 移動後のViewの4辺の位置を取得
+                    val viewLeftPosition: Int = view.left + (newPositionX - oldPositionX)
+                    val viewRightPoint: Int = viewLeftPosition + view.width
+                    val viewTopPosition: Int = view.top + (newPositionY - oldPositionY)
+                    val viewUnderPoint: Int = viewTopPosition + view.height
+
+                    // 画像の位置を設定する
+                    view.layout(viewLeftPosition, viewTopPosition, viewRightPoint, viewUnderPoint)
+                }
+
+                else -> {}
+            }
+
+            // タッチした位置を古い位置とする
+            oldPositionX = newPositionX
+            oldPositionY = newPositionY
+
+            return@setOnTouchListener true
+        }
+
         webViewButton.setOnClickListener {
             // 多言語で挨拶画面に遷移
             val intent = Intent(this, MainActivity::class.java)  //intentインスタンスの生成(第二引数は遷移先のktファイル名)
@@ -24,6 +56,6 @@ class WebViewActivity : AppCompatActivity() {
         val myWebView: WebView = findViewById(R.id.webView)
         myWebView.settings.javaScriptEnabled = true
         myWebView.loadUrl("https://x.com")
-
     }
+
 }
